d77e2ae768873512e155d3903fbbbeb7e2a4da45
espertixtilrex <101407886+espertixtilrex@users.noreply.github.com>
2022-03-13 01:53:24 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 global.DeveloperMode = 'true' //true Or false
-global.owner = [ '573155283012','573163664721','573213844393','573132536210','573132582315','5491173618515'] // Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
+global.owner = [ '573155283012','573163664721','573213844393','573132536210','573132582315','5491173618515','@'] // Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
 
 
 // Nota: Puedes contactarme si necesitas ayuda con algo al +5219996125657 (este es mi numero personal/real bloqueo personas que no sean temas de la instalación)
